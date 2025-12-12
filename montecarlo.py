507d2c3383fe117d9c169ae0326a66190667eca6
HenryJarrett <144489473+HenryJarrett@users.noreply.github.com>
2025-12-12 13:36:59 -0800

    Removed unused functions

--- montecarlo.py
+++ montecarlo.py
@@ -1,9 +1,11 @@
 import distributions
-#import matplotlib.pyplot as plt
 
 def aggregateClaims(count, severity):
+    # Generate random number of claims accoriding to count distribution
     num_claims = count.random()
     agg_claims = 0
+
+    # Generate random claim amounts according to severity distribution
     for i in range(num_claims):
         agg_claims += severity.random()
     return round(agg_claims)
@@ -11,20 +13,24 @@ def aggregateClaims(count, severity):
 def calculateTVaR(aggClaimDict, sampleSize, p):
     numClaims = 0
     i = 0
+    # Advance to the p percentile in the claim dictionary
     while numClaims < int(sampleSize*p):
         if i in aggClaimDict:
             numClaims += aggClaimDict[i]
         i += 1
+    # Value at risk is the p percentile of the aggregate claim dictionary
     VaR = i-1
     p = numClaims/sampleSize
     TVaR = 0
     cAbove = 0
+    # Iterate through the rest of the dictionary to compute Tail Value at Risk
     for j in range(i+1, max(aggClaimDict.keys())+1):
         if j in aggClaimDict:
             cAbove += aggClaimDict[j]
             TVaR += aggClaimDict[j]*j
+
+    # Mean loss given the loss is above the p percentile
     TVaR = TVaR/cAbove
-    # Decrement i to account for i += 1 at the end of each iteration
     return p, VaR, TVaR
 
 def calculateMeanMode(aggClaimDict, sampleSize):
@@ -32,63 +38,9 @@ def calculateMeanMode(aggClaimDict, sampleSize):
     mode = [0,0]
     for i in aggClaimDict.keys():
         mean += aggClaimDict[i]*i/sampleSize
+        # Find the most common value 
         if aggClaimDict[i] > mode[1]:
             mode[0] = i
             mode[1] = aggClaimDict[i]
     return mean, mode[0]
 
-def buildAggClaim(countDist, severityDist, sampleSize):
-    aggClaimDict = {}
-
-    # Run and log simulations
-    for i in range(sampleSize):
-        tempClaims = aggregateClaims(countDist, severityDist)
-        if tempClaims in aggClaimDict:
-            aggClaimDict[tempClaims] += 1
-        else:
-            aggClaimDict[tempClaims] = 1
-    
-    # Sort Dictionary by keys
-    return dict(sorted(aggClaimDict.items()))
-
-def sim(countDist, severityDist, sampleSize):
-    aggClaimDict = buildAggClaim(countDist, severityDist, sampleSize)
-    
-    # Calculate mean and mode
-    mean, mode = calculateMeanMode(aggClaimDict, sampleSize)
-    
-
-    # Calculate VaR for p = 0.9
-    p_90, VaR_90, TVaR_90 = calculateTVaR(aggClaimDict, sampleSize, 0.90)
-
-    # Calculate VaR for p = 0.95
-    p_95, VaR_95, TVaR_95 = calculateTVaR(aggClaimDict, sampleSize, 0.95)
-    
-    # Calculate VaR for p = 0.99
-    p_99, VaR_99, TVaR_99 = calculateTVaR(aggClaimDict, sampleSize, 0.99)
-
-    # Calculate VaR for p = 0.995
-    p_995, VaR_995, TVaR_995 = calculateTVaR(aggClaimDict, sampleSize, 0.995)
-
-
-    # Display Statistics
-    print("MEAN = " + str(mean))
-    print("MODE = " + str(mode))
-    print(str(p_90*100) + "% VaR = " + str(VaR_90) + " TVaR = " + str(TVaR_90))
-    print(str(p_95*100) + "% VaR = " + str(VaR_95) + " TVaR = " + str(TVaR_95))
-    print(str(p_99*100) + "% VaR = " + str(VaR_99) + " TVaR = " + str(TVaR_99))
-    print(str(p_995*100) + "% VaR = " + str(VaR_995) + " TVaR = " + str(TVaR_995))
-
-'''
-    # Plot frequency of aggregate claim amounts
-    plt.plot(aggClaimDict.keys(), aggClaimDict.values())
-    plt.title("Aggregate Claim Frequency n = " + str(sampleSize))
-    plt.xlabel("Aggregate Claim: " + severityDist.describe())
-    plt.ylabel("Frequency: " + countDist.describe())
-    plt.show()
-    #print(aggClaimDict)
-   ''' 
-
-#countDist = distributions.Poisson(5)
-#sevdist = distributions.Pareto(2, 3)
-#sim(countDist, sevdist, 100000)
